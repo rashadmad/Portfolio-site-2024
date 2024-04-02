99c13e1b25c4af33f0eb2093f505d58fe1c67369
rashadmad@gmail.com <rashadmadison@rashads-MBP.attlocal.net>
2024-04-01 22:02:56 -0400

    Add footer

--- .cache/page-ssr/routes/component---src-pages-index-jshead.js
+++ .cache/page-ssr/routes/component---src-pages-index-jshead.js
@@ -175,38 +175,46 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-const IndexPage = () => {
+const IndexPage = ({
+  title,
+  description,
+  img,
+  imgAlt,
+  tech,
+  live,
+  source
+}) => {
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_Banner__WEBPACK_IMPORTED_MODULE_1__["default"], {
     firstItem: "Grand re-opening!",
     secondItem: "Open to software engineering opportunities",
     buttonText: "Get in touch"
   }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-    className: "bg-white py-24 sm:py-32"
+    className: "bg-white py-24 sm:py-32 flex-grow"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     className: "mx-auto max-w-7xl px-6 lg:px-8"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     className: "mx-auto max-w-2xl lg:mx-0"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h2", {
     className: "text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
-  }, "From the blog"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", {
+  }, "Portfolio"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", {
     className: "mt-2 text-lg leading-8 text-gray-600"
-  }, "Learn how to grow your business with our expert advice.")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+  }, "Take a look at my work. Most of my work is created in Vanilla JavaScript. At the moment I am taking the time to update everything in a different frontend framework. I am discussing my journey in my blog you are welcome to join me in my jour")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     className: "mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3"
-  }, _data_json__WEBPACK_IMPORTED_MODULE_2__.map((img, imageAlt, tech, title, description) => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("article", {
-    key: _data_json__WEBPACK_IMPORTED_MODULE_2__.id,
+  }, _data_json__WEBPACK_IMPORTED_MODULE_2__.map(data => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("article", {
+    key: data.id,
     className: "flex max-w-xl flex-col items-start justify-between"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     className: "aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-    src: img,
-    alt: imageAlt,
+    src: data.img,
+    alt: data.imageAlt,
     className: "h-full w-full object-cover object-center lg:h-full lg:w-full"
   })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     className: "flex items-center gap-x-4 text-xs"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-    href: tech[0],
+    href: data.source,
     className: "relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-  }, title)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+  }, _data_json__WEBPACK_IMPORTED_MODULE_2__.title)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     className: "group relative"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", {
     className: "mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
@@ -214,9 +222,9 @@ const IndexPage = () => {
     href: "#"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
     className: "absolute inset-0"
-  }), title)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", {
+  }), data.title)), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", {
     className: "mt-5 line-clamp-3 text-sm leading-6 text-gray-600"
-  }, description))))))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+  }, data.description))))))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     class: "flex flex-col min-h-screen"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main", {
     class: "flex-grow"
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,52 +5,52 @@ import Data from './data.json'
 import Seo from "../components/seo"
 
 
-const IndexPage = () => {
+const IndexPage = ({ title, description, img, imgAlt, tech, live, source }) => {
+
   return (
-  <>
+  <div class="flex flex-col min-h-screen">
     <Banner firstItem="Grand re-opening!" secondItem="Open to software engineering opportunities" buttonText="Get in touch"></Banner>
-    <div className="bg-white py-24 sm:py-32">
+    <div className="main bg-white py-24 sm:py-32 flex-grow">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Portfolio</h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            Take a look at my work. Most of my work is created in Vanilla JavaScript. At the moment I am taking the time to update everything in a diffrent frontend framework. I am discussing 
-            my journey in my blog you are welcome to join me in my journey.
+            Take a look at my work. Most of my work is created in Vanilla JavaScript. At the moment I am taking the time to update everything in a different frontend framework. I am discussing 
+            my journey in my blog you are welcome to join me in my jour
           </p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {Data.map((img, imageAlt, tech, title, description) => (
-            <article key={Data.id} className="flex max-w-xl flex-col items-start justify-between">
+          {Data.map((data) => (
+            <article key={data.id} className="flex max-w-xl flex-col items-start justify-between">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
-                  src={img}
-                  alt={imageAlt}
+                  src={data.img}
+                  alt={data.imageAlt}
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
               </div>
               <div className="flex items-center gap-x-4 text-xs">
                 <a
-                  href={tech[0]}
+                  href={data.source}
                   className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
                 >
-                  {title}
+                  {Data.title}
                 </a>
               </div>
               <div className="group relative">
                 <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                   <a href='#'>
                     <span className="absolute inset-0" />
-                    {title}
+                    {data.title}
                   </a>
                 </h3>
-                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{description}</p>
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{data.description}</p>
               </div>
             </article>
           ))}
         </div>
       </div>
     </div>
-    <div class="flex flex-col min-h-screen">
     <main class="flex-grow">
     </main>
       <footer class="bg-gray-800 text-white py-4">
@@ -58,8 +58,7 @@ const IndexPage = () => {
               <p>&copy; 2024 Rashad J Madison. All rights reserved.</p>
           </div>
       </footer>
-    </div>
-  </>
+  </div>
   )
 }
 
